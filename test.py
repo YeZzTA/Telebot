9ca55e61d564125b510c2ae65fb546e7e3f82832
YeZzTA <47574106+YeZzTA@users.noreply.github.com>
2020-03-17 15:01:00 +0200

    Update test.py

--- test.py
+++ test.py
@@ -32,7 +32,7 @@ def lalala(message):
         if message.text == 'Пойду делать домашку':
             bot.send_message(message.chat.id, 'Удачи тебе! Про тренировки не забывай!')
         elif message.text == 'Кардио':
-            markup = types.InlineKeyboardMarkup(row_width=4)
+            markup = types.InlineKeyboardMarkup(row_width=5)
             pushup = types.InlineKeyboardButton("Взрывные отжимания", callback_data='pushup1')
             squat = types.InlineKeyboardButton("Взрывные приседания", callback_data='squat1')
 
